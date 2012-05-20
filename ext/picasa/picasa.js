4a87dcb3e8bec9a61f55de34f8468ff9dde3bb1e
Ziqi Jin <ziqi.jin@me.com>
2012-05-19 20:46:13 -0500

    picasa

--- ext/picasa/picasa.js
+++ ext/picasa/picasa.js
@@ -50,8 +50,19 @@ function auth()
 	var myWindow=window.open(googleLogin(),"mywindow","width=550,height=550");
 	myWindow.focus();
 }
+function getAccessToken()
+{
+	$.post('http://happybucket.com/ct/xt_custom_code.bix?c={CCBCE38D-2EA2-4919-B530-6E30FBA111F9}&event=google_access_token', {'code': 'kACAH-1Ng1SFvdjaJdMKxnajxxEwNaO2Op0jHXSTs9a7g8SMwBQ4EwJum7AOPkr2y_RMH3y9gAwzFzqwkZQYNIAjN2QnMxHGMTuoOxdbiWtwnQ9_pfY24MuG1s',
+	'client_id':'85959154006-m68f33t69ar0b4ttnp1u7sh94ceq9aot.apps.googleusercontent.com',
+	'client_secret':'X_Xa3yQb71J46q_5q3_HPlJ7',
+	'redirect_uri':'http%3A%2F%2Fhappybucket.com%2Fct%2Fxt_custom_code.bix%3Fc%3D%257BCCBCE38D-2EA2-4919-B530-6E30FBA111F9%257D%26event%3Dcallback',
+	'grant_type':'authorization_code'}, function(data, textStatus, xhr) {
+		air.trace(data);
+	});
+	
+}
 function googleLogin()
 {
-	var url="https://accounts.google.com/o/oauth2/auth?scope=https%3A%2F%2Fpicasaweb.google.com%2Fdata%2F&state=%2Fprofile&redirect_uri=http%3A%2F%2Fhappybucket.com%2Fct%2Fxt_custom_code.bix%3Fc%3D%257BCCBCE38D-2EA2-4919-B530-6E30FBA111F9%257D%26event%3Dcallback&response_type=code&client_id=85959154006-m68f33t69ar0b4ttnp1u7sh94ceq9aot.apps.googleusercontent.com";
+	var url="https://accounts.google.com/o/oauth2/auth?scope=https%3A%2F%2Fpicasaweb.google.com%2Fdata%2F&state=%2Fprofile&redirect_uri=http%3A%2F%2Fhappybucket.com%2Fct%2Fxt_custom_code.bix%3Fc%3D%257BCCBCE38D-2EA2-4919-B530-6E30FBA111F9%257D%26event%3Dcallback&response_type=code&client_id=85959154006-m68f33t69ar0b4ttnp1u7sh94ceq9aot.apps.googleusercontent.com&access_type=offline";
 return url;
 }
